c0d1298caaeb96f1097876bb64232eec503445fe
ChristopherHunt7 <christopherhunt7@live.com>
2013-07-16 22:22:44 -0700

    Added for-loop test to White

--- src/White.java
+++ src/White.java
@@ -1,5 +1,14 @@
 public class White{
-	public static void woof(){
-		System.out.println("Woof!");
+	public static void Woof(){
+		println("Woof!");
+		for(int i = 0; i > 2; i++){
+			String string1 = new String("Integer 'i': ");
+			String string2 = new String(Integer.toString(i));
+			String string3 = string1.concat(string2);
+			println(string3);
+		}
+	}
+	public static void println(String string){
+		System.out.println(string);
 	}
 }
