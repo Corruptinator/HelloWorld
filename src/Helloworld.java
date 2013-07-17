e7c2ba6c6574014392a33aec6ae030f69e6a81a7
Nathan McArthur <corruptinatorx01@gmail.com>
2013-07-16 21:32:21 -0700

    Commit #1

---
+++ src/Helloworld.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		// TODO Auto-generated method stub
+		System.out.println("Testing 123 123 123");
+		System.out.println("#DerpyNathan");
+		int dub = 14;
+		int dubish = 235;
+		double d = dub+dubish;
+		System.out.println(d);
+		System.out.println("Please enter your new number");
+		int derpy = Integer.parseInt(input.nextLine());
+		System.out.println(derpy);
+	}
+
+}
